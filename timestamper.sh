cec4d6bc6a8526ec539d5d80d3438abc8f082b51
Joseph <dingousa12@me.com>
2023-04-11 18:09:03 -0400

    rename file

--- timestamper.sh
+++ timestamper.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-# DIRECTORY="Screenshots"
-DIRECTORY="Video Clips"
-FILETYPE="webm"
+# Uncomment the directory you want the script to run in
+DIRECTORY="Screenshots"
+# DIRECTORY="Video Clips"
+
+# Uncomment the media filetype you want to update timestamps for
+# (or, if the one you need isn't here, just add it)
+FILETYPE="jpg"
+# FILETYPE="webm"
+# FILETYPE="mp4"
+
+
+#### Don't touch below unless changes are needed ####
 
 for folder in "./$DIRECTORY/"*; do
 
-  echo $folder
+  echo "Updating timestamps in $folder"
 
   for file in "$folder"/*."$FILETYPE"; do
 
-    echo $file
+    # echo $file
 
     # Extract the timestamp from the filename
     TIMESTAMP=$(basename "$file" ."$FILETYPE" | sed 's/.*_\(.*\)/\1/')
