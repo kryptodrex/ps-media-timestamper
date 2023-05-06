95134fc4829f901e4aa59d2cd9425f22643ae063
Joseph <dingousa12@me.com>
2023-05-06 13:54:33 -0400

    Updated to not need to pick file type or directories

--- timestamper.sh
+++ timestamper.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
-# Uncomment the directory you want the script to run in
-DIRECTORY="Screenshots"
-# DIRECTORY="Video Clips"
-
-# Uncomment the media filetype you want to update timestamps for
-# (or, if the one you need isn't here, just add it)
-FILETYPE="jpg"
-# FILETYPE="webm"
-# FILETYPE="mp4"
-
-
 #### Don't touch below unless changes are needed ####
+
+DIRECTORY="Media"
 
 for folder in "./$DIRECTORY/"*; do
 
+  echo ""
   echo "Updating timestamps in $folder"
+  echo ""
 
-  for file in "$folder"/*."$FILETYPE"; do
+  # for file in "$folder"/*."$FILETYPE"; do
+  for file in "$folder"/*; do
+    echo $file
 
-    # echo $file
+    # Extract the timestamp from the file name
+    TIMESTAMP=$(basename "$file" | grep -oE '[0-9]{14}')
+    echo "Extracted timestamp: $TIMESTAMP"
+    
+    if [[ -n $TIMESTAMP ]]; then
+      # Convert the timestamp to a format that touch can understand
+      TOUCH_TIMESTAMP=$(date -j -f "%Y%m%d%H%M%S" "${TIMESTAMP}" +"%Y%m%d%H%M.%S")
+      echo "Converted timestamp: $TOUCH_TIMESTAMP"
+      
+      # Update the timestamp metadata of the file
+      cp -a "$file" "$file.tmp" && mv "$file.tmp" "$file" && touch -mt "$TOUCH_TIMESTAMP" "$file"
+      echo "Timestamp updated"
+    else
+      echo "No timestamp found in file name"
+    fi
 
-    # Extract the timestamp from the filename
-    TIMESTAMP=$(basename "$file" ."$FILETYPE" | sed 's/.*_\(.*\)/\1/')
-    # echo $TIMESTAMP
-
-    # Convert the timestamp to a format that touch can understand
-    TOUCH_TIMESTAMP=$(date -j -f "%Y%m%d%H%M%S" "${TIMESTAMP}" +"%Y%m%d%H%M.%S")
-    # echo $TOUCH_TIMESTAMP
-
-    # Update the timestamp metadata of the image file
-    touch -m -t "$TOUCH_TIMESTAMP" "$file"
+    echo ""
 
   done
+
+  echo "Completed updating timestamps in $folder"
+  echo ""
 
 done
 
